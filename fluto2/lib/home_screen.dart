f0037b5c22e15b3b278de6fc4b23038479b56d9a
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-03-16 17:29:32 +0100

    FT02.5 Filtrage de liste

--- fluto2/lib/home_screen.dart
+++ fluto2/lib/home_screen.dart
@@ -6,32 +6,50 @@ import 'package:flutter/material.dart';
 import 'contact.dart';
 import 'contact_row.dart';
 
-class homeScreen extends StatelessWidget {
+
+
+class homeScreen extends StatefulWidget {
   const homeScreen ({Key? key, required String title}) : super(key: key);
 
   @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  var showFavorites = false;
+  void _toggleFavorites() => setState(() => showFavorites = !showFavorites);
+
+  @override
   Widget build(BuildContext context) {
+    final displayedContacts = [
+      for (var contact in defaultContacts)
+        if (!showFavorites || contact.isFavorite) contact
+    ];
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Contact list")),
+      appBar: AppBar(
+        title: const Text("Contact list"),
+        actions: [
+          IconButton(
+            icon: Icon(showFavorites ? Icons.star : Icons.star_border),
+            onPressed: _toggleFavorites,
+          ),
+        ],
+      ),
       body: Container(
         padding: const EdgeInsets.all(16.0),
         child: Center(
           child: SizedBox(
             width: 512.0,
             child: ListView.builder(
-              itemCount: defaultContacts.length,
-              itemBuilder: (context, index) {
-                log("index : $index");
-                return ContactRow(contact: defaultContacts[index]);
-              },
-
+              itemCount: displayedContacts.length,
+              itemBuilder: (context, index) =>
+                  ContactRow(contact: displayedContacts[index]),
             ),
           ),
         ),
       ),
     );
   }
-
-
 
 }
