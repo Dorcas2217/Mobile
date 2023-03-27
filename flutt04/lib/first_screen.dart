d74c40b1eb19c2284f3521194813fca47f988e89
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-03-27 12:14:42 +0200

    FT04.1 Premier écran

---
+++ flutt04/lib/first_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  var nbClicks = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("First screen")),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("Hello from first screen."),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => setState(() => nbClicks++),
+              child: const Text("click me"),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(
+                context,
+                "/second",
+                arguments: nbClicks,
+              ),
+              child: const Text("Go to second screen"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
