85950bed72c64fa2fd6d5fa8b60cc073e95ca64f
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-03-27 12:17:15 +0200

    FT04.2 Deuxième écran

---
+++ flutt04/lib/second_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final nbClicks = ModalRoute.of(context)!.settings.arguments as int;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second screen"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("Hello from second screen."),
+            const SizedBox(height: 16),
+            Text("There were $nbClicks clicks in the first page."),
+          ],
+        ),
+      ),
+    );
+  }
+}
