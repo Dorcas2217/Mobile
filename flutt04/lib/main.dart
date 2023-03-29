de4e41871b616344e0f95a52c0abbfe9640c44d9
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-03-29 17:47:29 +0200

    FT04.5 ChangeNotifierProvider

--- flutt04/lib/main.dart
+++ flutt04/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutt04/view_model/click_view_model.dart';
 import 'package:flutt04/views/second_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package: flutter/provider.dart';
 import 'views/first_screen.dart';
 
 void main() {
@@ -13,14 +14,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.purple),
-      initialRoute: "/first",
-      routes: {
-        "/first": (context) => const FirstScreen(),
-        "/second": (context) => const SecondScreen(),
-      },
+    return ChangeNotifierProvider<ClickViewModel>(
+      create: (context) => ClickViewModel(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        initialRoute: "/first",
+        routes: {
+          "/first": (context) => const FirstScreen(),
+          "/second": (context) => const SecondScreen(),
+        },
+      ),
     );
   }
+
 }
