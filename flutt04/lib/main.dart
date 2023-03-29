acb22f3c4a9bcbb4bf3b58787357c16310ef2d78
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-03-29 16:33:53 +0200

    FT04.4 Couche views

--- flutt04/lib/main.dart
+++ flutt04/lib/main.dart
@@ -1,18 +1,26 @@
-import 'package:flutt04/second_screen.dart';
+import 'package:flutt04/views/second_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'first_screen.dart';
+import 'views/first_screen.dart';
 
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(primarySwatch: Colors.blue),
-    initialRoute: "/first",
-    routes: {
-      "/first": (context) => const FirstScreen(),
-      "/second": (context) => const SecondScreen(),
-    },
-  );
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      initialRoute: "/first",
+      routes: {
+        "/first": (context) => const FirstScreen(),
+        "/second": (context) => const SecondScreen(),
+      },
+    );
+  }
 }
